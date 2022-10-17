832a8e4fcc12a8029215041dea965c993a371d63
1B _9_WK <114579256+Ocelot78@users.noreply.github.com>
2022-10-17 11:20:57 +0000

    cos

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ print("Witaj", name)
 time.sleep(3)
 clear()
 print("Ładowanie...")
+pytanie1 = input("Czy grasz w LoL: ")
+print("Wpisz z capslokiem TAK LUB NIE")
+if pytanie1 == ("TAK"):
+  print("Nie będziesz miał dziewczyny")
+elif pytanie1 == ("NIE"):
+  print("Będziesz miał dziewczyne")
